39507a5f5aeccbc84401de885bf7a540a2c669a6
alesky <alessandro.dottavio@gmail.com>
2020-03-26 14:43:10 +0100

    mail test

---
+++ cryptocurrencyalerting/src/test/java/it/spaghettisource/cryptocurrencyalerting/action/MailActionTest.java
@@ -0,0 +1,30 @@
+package it.spaghettisource.cryptocurrencyalerting.action;
+
+import org.junit.Test;
+
+import it.spaghettisource.cryptocurrencyalerting.action.MailAction.EncryptType;
+
+public class MailActionTest {
+
+	//confgure the provider to make the test or it will fail
+	//@Test
+	public void sendMail() {
+		
+		MailAction action = new MailAction();
+		
+		action.setHost("smtp.gmail.com");
+		action.setPort("587");
+		
+		action.setAuthentication("true");	//if set the autentication set also the user and pwd
+		action.setUsername("user@domain.com");
+		action.setPassword("pwd");
+
+		action.setSubject("subject");
+
+		action.setEncryptType(EncryptType.TLS);	//set the encrypt to use
+		
+		action.trigger("this is the content of the mail.... hello from cryptoallert test");
+		
+	}
+		
+}
