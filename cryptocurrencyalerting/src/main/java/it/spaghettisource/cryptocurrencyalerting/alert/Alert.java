c4a68389333f458ffd97515ddc1f24b36f101797
alesky <alessandro.dottavio@gmail.com>
2020-03-26 23:05:55 +0100

    base package and interface for the Alert

---
+++ cryptocurrencyalerting/src/main/java/it/spaghettisource/cryptocurrencyalerting/alert/Alert.java
@@ -0,0 +1,14 @@
+package it.spaghettisource.cryptocurrencyalerting.alert;
+
+/**
+ * Base interface for all the possible alerts
+ * 
+ * @author Alessandro
+ * @version 1.0
+ */
+public interface Alert {
+
+	
+	
+	
+}
