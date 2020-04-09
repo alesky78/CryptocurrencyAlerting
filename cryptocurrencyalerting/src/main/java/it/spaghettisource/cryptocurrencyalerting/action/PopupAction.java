049b47c9ee24f5080ae4e01ec7c8b4a64aaf55a3
alesky <alessandro.dottavio@gmail.com>
2020-04-09 15:05:55 +0200

    force the windows to go in foreground in case is not active

--- cryptocurrencyalerting/src/main/java/it/spaghettisource/cryptocurrencyalerting/action/PopupAction.java
+++ cryptocurrencyalerting/src/main/java/it/spaghettisource/cryptocurrencyalerting/action/PopupAction.java
@@ -2,6 +2,7 @@ package it.spaghettisource.cryptocurrencyalerting.action;
 
 import java.awt.Frame;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import org.slf4j.Logger;
@@ -20,7 +21,7 @@ import it.spaghettisource.cryptocurrencyalerting.ui.AppSwingUIManager;
 public class PopupAction extends AbstractAction {
 
 	static Logger log = LoggerFactory.getLogger(PopupAction.class);
-	
+
 
 	public PopupAction(ExceptionFactory exceptionFactory) {
 		super();
@@ -39,11 +40,11 @@ public class PopupAction extends AbstractAction {
 
 		Thread thread = new Thread(new OpenAsyncPopup(message), "Thread open popup");
 		thread.start();
-		
+
 	}
-	
-	
-	
+
+
+
 	class OpenAsyncPopup implements Runnable{
 
 		private String message;
@@ -58,15 +59,22 @@ public class PopupAction extends AbstractAction {
 		public void run() {
 			//send the message
 			Frame[] frames = Frame.getFrames();
-			
+
 			for (int i = 0; i < frames.length; i++) {
 				if(AppSwingUIManager.MAIN_FRAME_TITLE.equals(frames[i].getTitle())) {
-					
+
 					log.debug("open the popup");
+					
+					//move to foreground and deiconize
+					if (!frames[i].isActive()) {
+						frames[i].setState(JFrame.ICONIFIED);
+						frames[i].setState(JFrame.NORMAL);
+					}					
+
 					JOptionPane.showMessageDialog(frames[i],
-												  message,
-												  "Alert !!!!",
-												  JOptionPane.WARNING_MESSAGE);
+							message,
+							"Alert !!!!",
+							JOptionPane.WARNING_MESSAGE);
 				}
 			}
 
